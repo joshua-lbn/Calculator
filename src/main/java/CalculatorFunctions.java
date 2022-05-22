ae11ed5cd9302ae47171be2a764d167e0ce87bd2
Joshi <joshua.libon@yahoo.com>
2022-05-22 09:21:38 +0200

    Test

---
+++ src/main/java/CalculatorFunctions.java
@@ -0,0 +1,10 @@
+public class CalculatorFunctions {
+
+    public void update(String input){
+        switch(input){
+            case "0", "1", "2", "3" -> {
+               // TODO do sth
+            }
+        }
+    }
+}
