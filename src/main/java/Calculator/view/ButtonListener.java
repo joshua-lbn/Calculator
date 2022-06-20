1f37a924fe3b572ccd13332332ed0a63a24f4d4a
Joshi <joshua.libon@yahoo.com>
2022-06-20 20:01:28 +0200

    CalculatorState

--- src/main/java/Calculator/view/ButtonListener.java
+++ src/main/java/Calculator/view/ButtonListener.java
@@ -1,10 +1,7 @@
 package Calculator.view;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.LinkedList;
-import java.util.List;
+
 
 /*
  * ButtonListener-Klasse zum Auswerden der Interaktionen mit der grafischen Oberflaeche.
@@ -50,7 +47,6 @@ public class ButtonListener implements java.awt.event.ActionListener {
         * der View der Befehl gegeben, nach Änderung der Darstellung (durch den Controller) diese zu aktualisieren.
         * die weitere Ausfuehrung der Methode aus Effizienzgruenden abgebrochen.
         */
-
         for (int i = 0; i < jButtonsNumber.length; i++) {
             if (e.getSource() == jButtonsNumber[i]) {
                 view.Update("" + (i) + "");
@@ -80,6 +76,4 @@ public class ButtonListener implements java.awt.event.ActionListener {
             }
         }
     }
-
-
 }
