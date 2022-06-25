4b71903fdaca412de0d194e058c0c9b566339a60
JanM2005 <elp01940@gmail.com>
2022-06-25 19:44:19 +0200

    Verkürzen des Codes, eingehen auf Vorschläge des Kaisers und hinzufügen der Ans-Taste mit a, auch anderes denkbar

--- src/main/java/Calculator/view/ProcessKeyInput.java
+++ src/main/java/Calculator/view/ProcessKeyInput.java
@@ -10,7 +10,6 @@ import java.awt.event.KeyListener;
  */
 public class ProcessKeyInput implements KeyListener {
     private View view;
-    boolean shifttasteGedrueckt;
 
     public ProcessKeyInput(View v) {
         view = v;
@@ -18,33 +17,27 @@ public class ProcessKeyInput implements KeyListener {
 
     /* KeyPressed prueft die gedrückten Tasten, Aufruf beim Druecken einer Taste,
     * diese werden dann an die view weitergegeben und ausgegeben
-    * eventuell Wiederholung für 0 - 9, möglich?
      */
     public void keyPressed(KeyEvent e) {
-        //Zunächst überprüfen, ob Shift-Taste gedrückt, dann setzen unseres Parameters @Shifttaste
-        //auf true
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-            shifttasteGedrueckt = true;
-        }
         if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_7) {
             view.Update("/");
             view.UpdateView();
-        } else if (shifttasteGedrueckt && e.getKeyCode() == KeyEvent.VK_8) {
+        } else if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_8) {
             view.Update("(");
             view.UpdateView();
-        } else if (shifttasteGedrueckt && e.getKeyCode() == KeyEvent.VK_9) {
+        } else if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_9) {
             view.Update(")");
             view.UpdateView();
-        } else if (shifttasteGedrueckt && e.getKeyCode() == KeyEvent.VK_PLUS) {
+        } else if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_PLUS) {
             view.Update("*");
             view.UpdateView();
         } else if (e.getKeyCode() == KeyEvent.VK_PLUS) {
             view.Update("+");
             view.UpdateView();
         } else if (e.getKeyCode() == KeyEvent.VK_MINUS) {
-            view.Update("*");
+            view.Update("-");
             view.UpdateView();
-        } else if (shifttasteGedrueckt && e.getKeyCode() == KeyEvent.VK_0) {
+        } else if (e.isShiftDown() && e.getKeyCode() == KeyEvent.VK_0) {
             view.Update("=");
             view.UpdateView();
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
@@ -53,40 +46,37 @@ public class ProcessKeyInput implements KeyListener {
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             view.Update("<-");
             view.UpdateView();
-        }
-        else if (e.getKeyCode() == KeyEvent.VK_0) {
+        } else if (e.getKeyCode() == KeyEvent.VK_0 || e.getKeyCode() == KeyEvent.VK_NUMPAD0) {
             view.Update("" + 0 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_1) {
+        } else if (e.getKeyCode() == KeyEvent.VK_1 || e.getKeyCode() == KeyEvent.VK_NUMPAD1) {
             view.Update("" + 1 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_2) {
+        } else if (e.getKeyCode() == KeyEvent.VK_2 || e.getKeyCode() == KeyEvent.VK_NUMPAD2) {
             view.Update("" + 2 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_3) {
+        } else if (e.getKeyCode() == KeyEvent.VK_3 || e.getKeyCode() == KeyEvent.VK_NUMPAD3) {
             view.Update("" + 3 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_4) {
+        } else if (e.getKeyCode() == KeyEvent.VK_4 || e.getKeyCode() == KeyEvent.VK_NUMPAD4) {
             view.Update("" + 4 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_5) {
+        } else if (e.getKeyCode() == KeyEvent.VK_5 || e.getKeyCode() == KeyEvent.VK_NUMPAD5) {
             view.Update("" + 5 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_6) {
+        } else if (e.getKeyCode() == KeyEvent.VK_6 || e.getKeyCode() == KeyEvent.VK_NUMPAD6) {
             view.Update("" + 6 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_7) {
+        } else if (e.getKeyCode() == KeyEvent.VK_7 || e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
             view.Update("" + 7 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_8) {
+        } else if (e.getKeyCode() == KeyEvent.VK_8 || e.getKeyCode() == KeyEvent.VK_NUMPAD8) {
             view.Update("" + 8 + "");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_9) {
+        } else if (e.getKeyCode() == KeyEvent.VK_9 || e.getKeyCode() == KeyEvent.VK_NUMPAD9) {
             view.Update("" + 9 + "");
             view.UpdateView();
-        }
-        // Restliche Buttons
-        else if (e.getKeyCode() == KeyEvent.VK_S) {
+        } else if (e.getKeyCode() == KeyEvent.VK_S) {
             view.Update("sin(");
             view.UpdateView();
         } else if (e.getKeyCode() == KeyEvent.VK_C) {
@@ -104,69 +94,22 @@ public class ProcessKeyInput implements KeyListener {
         } else if (e.getKeyCode() == KeyEvent.VK_DELETE) {
             view.Update("AC");
             view.UpdateView();
-        }   else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             view.Update("=");
             view.UpdateView();
-        }
-        //Ab hier wird die Shift-Taste genutzt
-        //NumPad
-        else if (e.getKeyCode() == KeyEvent.VK_NUMPAD0) {
-            view.Update("" + 0 + "");
+        } else if (e.getKeyCode() == KeyEvent.VK_DEAD_CIRCUMFLEX) {
+            view.Update("x^");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD1) {
-            view.Update("" + 1 + "");
+        } else if (e.getKeyCode() == KeyEvent.VK_COMMA) {
+            view.Update(",");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD2) {
-            view.Update("" + 2 + "");
+        } else if(e.getKeyCode() == KeyEvent.VK_A) {
+            view.Update("Ans");
             view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD3) {
-            view.Update("" + 3 + "");
-            view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD4) {
-            view.Update("" + 4 + "");
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD5) {
-            view.Update("" + 5 + "");
-            view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD6) {
-            view.Update("" + 6 + "");
-            view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
-            view.Update("" + 7 + "");
-            view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD8) {
-            view.Update("" + 8 + "");
-            view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_NUMPAD9) {
-            view.Update("" + 9 + "");
-            view.UpdateView();
-        } else if (e.getKeyCode() == KeyEvent.VK_CIRCUMFLEX) {
-            view.Update("^x^");
-            view.UpdateView();
-        }
-        //Exponentialrechnung?
-        //auch mit OpenBrackets/Close möglich, aber erkennen möglich, dann ohne den Wahrheitswert
-    }
-
-    //wenn die Shift-Taste losgelassen wird
-    public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-            shifttasteGedrueckt = false;
         }
     }
-
     //nicht nötig
-    public void keyTyped(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
-            //shifttasteGedrueckt = !shifttasteGedrueckt;
-        }
-    }
+  public void keyReleased(KeyEvent e) {}
+    //nicht nötig
+    public void keyTyped(KeyEvent e) {}
 }
-
-/* ab hier löschen  public static void main(String[] main)
-{
-    //stellt einen KeyListener her und übergibt diesen dem (Fenster???)
-    ProcessKeyInput p = new ProcessKeyInput();
-    view.addKeyListener(p); //bei Frame, bei uns?
-}
-}
-*/
