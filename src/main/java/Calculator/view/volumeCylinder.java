8097b8c10379b02de670dfb0605ccbb927dbd566
Joshi <joshua.libon@yahoo.com>
2022-07-09 15:34:58 +0200

    Volumen Zylinder

--- src/main/java/Calculator/view/volumeCylinder.java
+++ src/main/java/Calculator/view/volumeCylinder.java
@@ -5,52 +5,43 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class volumeCuboid extends JRootPane {
+public class volumeCylinder extends JFrame {
     private javax.swing.JPanel jpanel1 = new javax.swing.JPanel();
     private javax.swing.JButton calculate = new javax.swing.JButton();
-    private javax.swing.JTextField lengthTextField = new javax.swing.JTextField();
-    private javax.swing.JTextField widthTextField = new javax.swing.JTextField();
+    private javax.swing.JTextField radiusTextField = new javax.swing.JTextField();
     private javax.swing.JTextField heightTextField = new javax.swing.JTextField();
-    private javax.swing.JLabel length = new javax.swing.JLabel();
-    private javax.swing.JLabel width = new javax.swing.JLabel();
+    private javax.swing.JLabel radius = new javax.swing.JLabel();
     private javax.swing.JLabel height = new javax.swing.JLabel();
     private javax.swing.JLabel volume = new javax.swing.JLabel();
     private javax.swing.JLabel volumeCalculate = new javax.swing.JLabel();
 
     private javax.swing.JLabel spacer1 = new javax.swing.JLabel();
     private javax.swing.JLabel spacer2 = new javax.swing.JLabel();
-    private javax.swing.JLabel spacer3 = new javax.swing.JLabel();
 
-    public volumeCuboid () {
+    public volumeCylinder () {
         //setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        jpanel1.setLayout(new GridLayout(4,3));
+        jpanel1.setLayout(new GridLayout(3,3));
 
         //jpanel1 = new javax.swing.JPanel();
-        length = new javax.swing.JLabel("Länge:");
-        lengthTextField = new javax.swing.JTextField();
+        radius = new javax.swing.JLabel("Radius:");
+        radiusTextField = new javax.swing.JTextField();
         spacer1 = new javax.swing.JLabel();
-        width = new javax.swing.JLabel("Breite:");
-        widthTextField = new javax.swing.JTextField();
-        spacer2 = new javax.swing.JLabel();
         height = new javax.swing.JLabel("Höhe:");
         heightTextField = new javax.swing.JTextField();
         calculate = new javax.swing.JButton("OK");
         volume = new javax.swing.JLabel("Volumen");
         volumeCalculate = new javax.swing.JLabel();
-        spacer3 = new javax.swing.JLabel();
+        spacer2 = new javax.swing.JLabel();
         //ggf. noch set Size
-        jpanel1.add(length);
-        jpanel1.add(lengthTextField);
+        jpanel1.add(radius);
+        jpanel1.add(radiusTextField);
         jpanel1.add(spacer1);
-        jpanel1.add(width);
-        jpanel1.add(widthTextField);
-        jpanel1.add(spacer2);
         jpanel1.add(height);
         jpanel1.add(heightTextField);
         jpanel1.add(calculate);
         jpanel1.add(volume);
         jpanel1.add(volumeCalculate);
-        jpanel1.add(spacer3);
+        jpanel1.add(spacer2);
         this.getContentPane().add(jpanel1);
         //pack();
         setVisible(true);
@@ -60,14 +51,14 @@ public class volumeCuboid extends JRootPane {
             public void actionPerformed(ActionEvent e) {
                 // Noch prüfen Buchstaben + leere Eingabe
                 // Parse Double, da nicht möglich String direkt in Zahl umwandeln
-                double volumeLength = Double.parseDouble(lengthTextField.getText().replace(',','.'));
-                double volumeWidth = Double.parseDouble(widthTextField.getText().replace(',','.'));
+                double volumeRadius = Double.parseDouble(radiusTextField.getText().replace(',','.'));
                 double volumeHeight = Double.parseDouble(heightTextField.getText().replace(',','.'));
-                if (volumeLength <= 0 || volumeWidth <= 0 || volumeHeight <= 0) {
-                    volumeCalculate.setText("Ung\u00FCltige Eingabe");
+                if (volumeRadius <= 0 || volumeHeight <= 0) {
+                    volumeCalculate.setText("ungültige Eingabe");
                 }
                 else {
-                    double volumeResult = volumeLength * volumeWidth * volumeHeight;
+                    double volumeResult = Math.PI * volumeRadius * volumeRadius * volumeHeight;
+                    System.out.println(""+volumeResult);
                     double volume = (double) (Math.round(volumeResult * 100)) / 100;
                     String volumeString = (Double.toString(volume)).replace ('.', ',');
                     volumeCalculate.setText(volumeString);
@@ -80,7 +71,7 @@ public class volumeCuboid extends JRootPane {
         this.setVisible(true);
     }
     public static void main(String[] args) {
-        new volumeCuboid().setVisible(true);
+        new volumeCone().setVisible(true);
     }
 
 
